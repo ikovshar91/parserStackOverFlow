5acd0f053f0b65a8be09cbf1ecbb9ae33cea96c1
IR_Kovshar <ikovshar91@gmail.com>
2021-09-02 17:05:59 +0600

    Commit#3

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,16 +1,18 @@
 package main;
 
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.StackOverFlowServlet;
+import servlets.AsyncServer;
 
 public class Main {
     public static void main(String[] args) {
         ServletContextHandler context = new ServletContextHandler(1);
-        context.addServlet(new ServletHolder(new StackOverFlowServlet()), "/*");
+        context.addServlet(new ServletHolder(new AsyncServer()), "/*");
 
         Server server = new Server(8080);
+
         server.setHandler(context);
 
             try {
--- src/main/java/servlets/StackOverFlowServlet.java
+++ src/main/java/servlets/StackOverFlowServlet.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import json.Item;
 import json.Root;
 import json.Error;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,56 +15,64 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.BitSet;
 import java.util.List;
 import java.util.zip.GZIPInputStream;
 
 public class StackOverFlowServlet extends HttpServlet {
+    private String tag;
+
+    public void setTag(String tag){
+        this.tag = tag;
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
-        Gson gson = new Gson();
-        String tag = req.getParameter("tag");
-        try {
-            if (tag == null) {
-                resp.setStatus(404);
-            } else {
-                URL url = new URL("https://api.stackexchange.com/2.2/search?pagesize=100&order=desc&sort=creation&tagged=" + tag + "&site=stackoverflow");
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            Gson gson = new Gson();
+                try {
+                    if (tag == null) {
+                        resp.setStatus(404);
+                    } else {
+                        URL url = new URL("https://api.stackexchange.com/2.2/search?pagesize=100&order=desc&sort=creation&tagged=" + tag + "&site=stackoverflow");
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("Content-Type", "application/json");
+                        connection.setRequestMethod("GET");
+                        connection.setRequestProperty("Content-Type", "application/json");
 
-                GZIPInputStream gis = new GZIPInputStream(connection.getInputStream());
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gis, StandardCharsets.UTF_8));
+                        GZIPInputStream gis = new GZIPInputStream(connection.getInputStream());
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gis, StandardCharsets.UTF_8));
 
-                String inputLine;
+                        String inputLine;
 
-                StringBuilder stringBuilder = new StringBuilder();
+                        StringBuilder stringBuilder = new StringBuilder();
 
-                while ((inputLine = bufferedReader.readLine()) != null) {
-                    stringBuilder.append((inputLine));
-                }
+                        while ((inputLine = bufferedReader.readLine()) != null) {
+                            stringBuilder.append((inputLine));
+                        }
 
-                bufferedReader.close();
+                        bufferedReader.close();
 
-                Root root = gson.fromJson(stringBuilder.toString(), Root.class);
+                        Root root = gson.fromJson(stringBuilder.toString(), Root.class);
 
-                List<Boolean> isAnswered = new ArrayList<>();
-                for (Item root1 : root.items) {
-                    if(root1.is_answered){
-                        isAnswered.add(true);
+                        List<Boolean> isAnswered = new ArrayList<>();
+                        for (Item root1 : root.items) {
+                            if (root1.is_answered) {
+                                isAnswered.add(true);
+                            }
+                        }
+                        resp.getWriter().printf("\"%s\":{ \"total:\": %s, \"answered\": %s}\n", tag, gson.toJson(root.items.size()), isAnswered.size());
                     }
-                }
-                resp.getWriter().printf("{ \n \"%s\" { \"total:\": %s, \"answered\": %s} \n}" , tag, gson.toJson(root.items.size()), isAnswered.size());
 
-            }
-        } catch (Exception e){
+            } catch (Exception e){
             e.printStackTrace();
             Error error = new Error(String.format("Kakaya-to huynya proizoshla: %s", e.getMessage()));
             resp.getWriter().print(gson.toJson(error));
             resp.setStatus(500);
         }
     }
-}
+ }
+
+
+
+
+
