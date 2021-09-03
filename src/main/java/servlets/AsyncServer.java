16784a3fd6d5498266dc01a70407bc300c12648f
IR_Kovshar <ikovshar91@gmail.com>
2021-09-03 14:28:29 +0600

    Commit#7

--- src/main/java/servlets/AsyncServer.java
+++ src/main/java/servlets/AsyncServer.java
@@ -15,6 +15,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.zip.GZIPInputStream;
 
@@ -24,9 +26,28 @@ public class AsyncServer extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final AsyncContext ctxt = req.startAsync();
-        String [] tags = req.getParameterValues("tag");
-        // Какой -то тег
-        String tag = null;
+        List<String> tags = Arrays.asList(req.getParameterValues("tag"));
+
+
+        //При написании такой конструкции на вызове метода getStackOverFlow требует его обернуть в трай блок и далее подобная ебанина
+        List<CompletableFuture<Root>> futures = tags.stream().map(tag -> CompletableFuture.supplyAsync(() -> {
+            try {
+                return getStackOverFlow(tag);
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+            return null;
+        }));
+
+
+        //А дэдлок в моей голове это вопрос того где правильно вытаскивать этот Root, в реализации метода run, т.к. он там работает непосредственно с каждым рутом в отдельности
+        // но в то же время нужно вне реализации метода нужно вытащить эти футуры и подставить к каждой этот метод ран для параллелизма
+        //ну либо я даунич и неправильно понимаю для чего вообще нужен этот CompletableFuture
+
+
+
+        //List<Root> roots = CompletableFuture.allOf(futures);
+
             ctxt.start(new Runnable() {
                            @Override
                            public void run() {
