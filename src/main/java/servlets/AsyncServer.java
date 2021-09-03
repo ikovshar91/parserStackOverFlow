48ca9a10e864b9675c006a084d2ee307481629b9
IR_Kovshar <ikovshar91@gmail.com>
2021-09-03 14:22:01 +0600

    Commit#6

--- src/main/java/servlets/AsyncServer.java
+++ src/main/java/servlets/AsyncServer.java
@@ -1,27 +1,39 @@
 package servlets;
 
+import com.google.gson.Gson;
+import json.Root;
+
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
-@WebServlet(urlPatterns={"/asyncioservlet"}, asyncSupported=true)
+import java.util.concurrent.CompletableFuture;
+import java.util.zip.GZIPInputStream;
+
+
 public class AsyncServer extends HttpServlet {
+    Gson gson = new Gson();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final AsyncContext ctxt = req.startAsync();
-        String[] tags = req.getParameterValues("tag");
-
-        for (String tag : tags) {
+        String [] tags = req.getParameterValues("tag");
+        // Какой -то тег
+        String tag = null;
             ctxt.start(new Runnable() {
                            @Override
                            public void run() {
                                try {
                                    StackOverFlowServlet servlet = new StackOverFlowServlet();
-                                   servlet.setTag(tag);
+                                   Root root = getStackOverFlow(tag);
+                                   servlet.setParameter(tag, gson, root);
                                    servlet.doGet(req, resp);
                                    ctxt.complete();
                                } catch (ServletException | IOException e) {
@@ -30,6 +42,29 @@ public class AsyncServer extends HttpServlet {
                            }
                        }
             );
-        }
+
     }
+
+    public Root getStackOverFlow(String tag) throws IOException {
+        URL url = new URL("https://api.stackexchange.com/2.2/search?pagesize=100&order=desc&sort=creation&tagged=" + tag + "&site=stackoverflow");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type", "application/json");
+
+        GZIPInputStream gis = new GZIPInputStream(connection.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gis, StandardCharsets.UTF_8));
+
+        String inputLine;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while ((inputLine = bufferedReader.readLine()) != null) {
+            stringBuilder.append((inputLine));
+        }
+        bufferedReader.close();
+
+        return gson.fromJson(stringBuilder.toString(), Root.class);
+    }
+
 }
--- src/main/java/servlets/StackOverFlowServlet.java
+++ src/main/java/servlets/StackOverFlowServlet.java
@@ -8,22 +8,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.GZIPInputStream;
 
 public class StackOverFlowServlet extends HttpServlet {
     private String tag;
-    Gson gson = new Gson();
+    private Gson gson;
+    private Root root;
 
-    public void setTag(String tag){
+    public void setParameter(String tag, Gson gson, Root root){
         this.tag = tag;
+        this.gson = gson;
+        this.root = root;
     }
 
     @Override
@@ -34,8 +31,6 @@ public class StackOverFlowServlet extends HttpServlet {
             if (tag == null) {
                 resp.setStatus(404);
             } else {
-
-                Root root = getStackOverFlow(tag);
                 List<Boolean> isAnswered = new ArrayList<>();
                 for (Item root1 : root.items) {
                     if (root1.is_answered) {
@@ -44,7 +39,6 @@ public class StackOverFlowServlet extends HttpServlet {
                 }
                 resp.getWriter().printf("\"%s\":{ \"total:\": %s, \"answered\": %s}\n", tag, gson.toJson(root.items.size()), isAnswered.size());
             }
-
         } catch (Exception e){
             e.printStackTrace();
             Error error = new Error(String.format("Kakaya-to huynya proizoshla: %s", e.getMessage()));
@@ -52,31 +46,6 @@ public class StackOverFlowServlet extends HttpServlet {
             resp.setStatus(500);
         }
     }
-
-    public Root getStackOverFlow(String tag) throws IOException {
-        URL url = new URL("https://api.stackexchange.com/2.2/search?pagesize=100&order=desc&sort=creation&tagged=" + tag + "&site=stackoverflow");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("Content-Type", "application/json");
-
-        GZIPInputStream gis = new GZIPInputStream(connection.getInputStream());
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gis, StandardCharsets.UTF_8));
-
-        String inputLine;
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-        while ((inputLine = bufferedReader.readLine()) != null) {
-            stringBuilder.append((inputLine));
-        }
-
-        bufferedReader.close();
-
-        return gson.fromJson(stringBuilder.toString(), Root.class);
-    }
-
-
 
 
 }
